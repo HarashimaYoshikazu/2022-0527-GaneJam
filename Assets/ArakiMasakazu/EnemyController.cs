ce035d37d2ad7a41d5b9d11b9c447675cd4b787a
Roku <mr558869193@gmail.com>
2022-05-27 14:05:36 +0900

    no message

--- Assets/ArakiMasakazu/EnemyController.cs
+++ Assets/ArakiMasakazu/EnemyController.cs
@@ -7,7 +7,6 @@ public class EnemyController : MonoBehaviour
     [SerializeField] float _speed = 3f; 
     Rigidbody2D _rb = default;
     Vector2 _input = Vector2.zero;
-    [SerializeField] Collider2D wallChaek;
     GameObject player = default;
 
     // Start is called before the first frame update
@@ -20,15 +19,19 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 enemypos = this.transform.position;
-        Vector3 playerpos = player.transform.position;
-        float dis = Vector3.Distance(enemypos, playerpos);
-
-        if (dis < 10)
+        if(player)
         {
-            _input = new Vector2(_speed, _rb.velocity.y);
-            _rb.velocity = _input;
+            Vector3 enemypos = this.transform.position;
+            Vector3 playerpos = player.transform.position;
+            float dis = Vector3.Distance(enemypos, playerpos);
+
+            if (dis < 10)
+            {
+                _input = new Vector2(_speed, _rb.velocity.y);
+                _rb.velocity = _input;
+            }
         }
+
         
     }
 
