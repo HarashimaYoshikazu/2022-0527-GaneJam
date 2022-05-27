22b38abcbf699c3ed2d68a7eb4b646df92a1c453
DESKTOP-J3CLVL8\vantan <0xc32525105508a@ezweb.ne.jp>
2022-05-27 13:12:45 +0900

    敵の移動

--- Assets/ArakiMasakazu/EnemyController.cs
+++ Assets/ArakiMasakazu/EnemyController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] float _speed = 3f;
+    [SerializeField] float _speed = 3f; 
     Rigidbody2D _rb = default;
     Vector2 _input = Vector2.zero;
+    [SerializeField] Collider2D wallChaek;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +18,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _input.x = _speed;
-        Debug.Log(_input);
-        _rb.velocity= _input.normalized;
+        //_input.x = _speed;
+        //Debug.Log(_input);
+        //_rb.velocity= _input.normalized;
+
+        _input = new Vector2(_speed, _rb.velocity.y);
+        _rb.velocity = _input;
     }
 }
