1949298bf6882aaa53cb4e902164dd955f7f269d
Aiba_0325 <bantan.ab325@gmail.com>
2022-05-27 12:05:34 +0900

    移動とジャンプ

---
+++ Assets/AibaRui/PlayerMove.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float _speed = 10f;
+    [SerializeField] float _jumppower = 10f;
+
+    int _jumpcount=0;
+    private bool _jump = false;
+    Rigidbody2D _rb = default;
+    Animator _anim;
+
+    private bool _ground = false;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         Move();
+        Jump();
+
+        //if(Input.GetButtonDown("Jump"))
+        //{
+        //    if (_jumpcount < 2)
+        //    {
+        //        _jump = true;
+        //    }
+
+        //}
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+        //if(_jump)
+        //{
+        //    _rb.AddForce(Vector2.up * _jumppower, ForceMode2D.Impulse);
+            
+        //    _jumpcount++;
+        //    _jump = false;
+        //}
+
+    }
+
+
+     void Move()
+    {
+        float _h = Input.GetAxisRaw("Horizontal");
+
+
+        Vector2 velo = new Vector2(_h * _speed, _rb.velocity.y);
+        _rb.velocity = velo;
+
+        if(_h !=0)
+        {
+            if(_h<0)
+            {
+                this.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                this.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump")&&_ground)
+        {
+            _rb.AddForce(Vector2.up * _jumppower, ForceMode2D.Impulse);
+            _ground = false;
+        }
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            _ground = true;
+           // _jumpcount = 0;
+        }
+
+    }
+
+}
+
