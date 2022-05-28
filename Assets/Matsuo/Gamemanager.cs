56d039e24b0c23007903b8bd7e6890e1230cb5ca
Roku <mr558869193@gmail.com>
2022-05-28 10:55:37 +0900

    no message

---
+++ Assets/Matsuo/Gamemanager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamemanager : MonoBehaviour
+{
+    [SerializeField] PlayerPalam playerstate;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject gameover;
+    bool isDeath = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //isDeath = playerstate.IsDeath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDeath = playerstate.IsDeath;
+        if (isDeath)
+        {
+            gameover.SetActive(true);
+            player.SetActive(false);
+        }
+        
+
+        if (gameover.activeSelf)
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene($"Title");
+
+            }
+        }
+    }
+}
